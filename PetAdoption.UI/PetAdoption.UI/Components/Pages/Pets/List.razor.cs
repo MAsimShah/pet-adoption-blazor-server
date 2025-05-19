81043e13b31b0085430cea14d6a7e67e9b17cea5
MAsimShah <shahasim190@gmail.com>
2025-05-19 22:58:08 +0500

    add pet modal and grid

---
+++ PetAdoption.UI/PetAdoption.UI/Components/Pages/Pets/List.razor.cs
@@ -0,0 +1,46 @@
+﻿using BlazorBootstrap;
+using PetAdoption.UI.Components.Models;
+
+namespace PetAdoption.UI.Components.Pages.Pets
+{
+    public partial class List
+    {
+        private List<PetViewModel> pets = new()
+    {
+        new PetViewModel { Name = "Buddy", Age = 12 },
+        new PetViewModel { Name = "Luna", Age = 09},
+        new PetViewModel { Name = "Max", Age = 56 }
+    };
+
+        private Modal modal;
+        private PetViewModel petViewModel = new PetViewModel();
+
+        private async Task OnShowModal()
+        {
+            await modal.ShowAsync();
+        }
+
+        private async Task OnHideModal()
+        {
+            await modal.HideAsync();
+        }
+
+        private void HandleValidSubmit()
+        {
+          //  var response = await Http.PostAsJsonAsync("AddPet", petViewModel);
+            var response = petViewModel;
+
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    // Optionally show a success message, close modal, etc.
+            //    Console.WriteLine("Pet added successfully.");
+            //    modal.Hide();
+            //}
+            //else
+            //{
+            //    // Handle errors (e.g., log or show to user)
+            //    Console.WriteLine("Error: " + response.ReasonPhrase);
+            //}
+        }
+    }
+}
