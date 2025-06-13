c79b84e291f60695a537ef2be10faf10a1e91962
MAsimShah <shahasim190@gmail.com>
2025-06-13 20:51:50 +0500

    added login page

---
+++ PetAdoption.UI/PetAdoption.UI/Components/Pages/Auth/Login.razor.cs
@@ -0,0 +1,39 @@
+﻿using BlazorBootstrap;
+using PetAdoption.UI.Components.Models;
+
+namespace PetAdoption.UI.Components.Pages.Auth
+{
+    public partial class Login
+    {
+        private LoginViewModel modal = new LoginViewModel();
+
+        private async Task LoginUser()
+        {
+            try
+            {
+                PreloadService.Show();
+
+                HttpResponseMessage? response = await Http.PostAsJsonAsync("/api/Auth/Login", modal);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    ToastService.Notify(new ToastMessage(ToastType.Danger, $"{modal.Email} not login successfully! Please try again"));
+                    return;
+                }
+
+                ToastService.Notify(new ToastMessage(ToastType.Success, $"{modal.Email} user not login successfully"));
+
+                _Naivigation.NavigateTo("/", true);
+            }
+            catch (Exception ex)
+            {
+                PreloadService.Hide();
+                ToastService.Notify(new ToastMessage(ToastType.Danger, $"Something went wrong!"));
+            }
+            finally
+            {
+                PreloadService.Hide();
+            }
+        }
+    }
+}
