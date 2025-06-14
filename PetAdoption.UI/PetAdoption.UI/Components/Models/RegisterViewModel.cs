2ed2e2c3d0ad0c0ade6b74202719c02469352825
MAsimShah <shahasim190@gmail.com>
2025-06-14 14:07:59 +0500

    register user page code

---
+++ PetAdoption.UI/PetAdoption.UI/Components/Models/RegisterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetAdoption.UI.Components.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Phone# is required.")]
+        [RegularExpression(@"^\+?[0-9]{10,15}$", ErrorMessage = "Enter a valid phone#.")]
+        public string PhoneNumber { get; set; }
+
+        public Gender Gender { get; set; } = Gender.Male;
+    }
+
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+}
