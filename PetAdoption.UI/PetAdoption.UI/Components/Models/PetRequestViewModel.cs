7ca362e1ff51a62c42995102e7e9a27ef9cbb78b
MAsimShah <shahasim190@gmail.com>
2025-07-21 14:52:22 +0500

    initial changes of request of pets

---
+++ PetAdoption.UI/PetAdoption.UI/Components/Models/PetRequestViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace PetAdoption.UI.Components.Models
+{
+    public class PetRequestViewModel
+    {
+        public int Id { get; set; }
+
+        public int PetId { get; set; }         // FK to Pet
+        public int UserId { get; set; }        // FK to User making the request
+
+        public DateTime RequestDate { get; set; } = DateTime.UtcNow;
+
+        public string Message { get; set; }    // Optional message by user
+
+        public RequestStatus Status { get; set; } = RequestStatus.Pending;
+    }
+
+    public enum RequestStatus
+    {
+        Pending,
+        Approved,
+        Rejected,
+        Cancelled
+    }
+}
---
+++ PetAdoption.UI/PetAdoption.UI/Components/Pages/PetRequests/List.razor.cs
@@ -0,0 +1,141 @@
+﻿using MudBlazor;
+using PetAdoption.UI.Components.Models;
+using PetAdoption.UI.Components.Models.APIModels;
+using PetAdoption.UI.Components.Pages.Pets;
+
+namespace PetAdoption.UI.Components.Pages.PetRequests
+{
+    public partial class List
+    {
+        private List<PetViewModel> petsList = new();
+        DialogOptions dialogOptions = new DialogOptions
+        {
+            BackgroundClass = "my-custom-class",
+            MaxWidth = MaxWidth.ExtraLarge,
+            Position = DialogPosition.Center,
+            FullWidth = true,
+        };
+
+        #region lifecycles
+
+        protected override async Task OnInitializedAsync()
+        {
+            await RefreshGrid();
+        }
+
+        #endregion lifecycles
+
+        #region methods
+
+        async Task EditPet(int petId)
+        {
+            Loader.Show();
+
+            try
+            {
+                var result = await petAPI.GetPetAsync(petId);
+
+                if (result is null)
+                {
+                    Snackbar.Add("Not fetched pet info", Severity.Error);
+                    return;
+                }
+
+                PetViewModel model = new(result);
+
+                var parameters = new DialogParameters<AddRequest> { { x => x.EditModel, model } };
+                var dialog = await DialogService.ShowAsync<AddRequest>("Edit new Pet", parameters, dialogOptions);
+                var dialogResult = await dialog.Result;
+
+                if (dialogResult != null && !dialogResult.Canceled)
+                {
+                    await RefreshGrid();
+                }
+            }
+            catch (Exception)
+            {
+                Loader.Hide();
+                Snackbar.Add("Not fetched pet info", Severity.Error);
+            }
+            finally
+            {
+                Loader.Hide();
+            }
+        }
+
+        private async Task AddPetAsync()
+        {
+            var dialog = await DialogService.ShowAsync<AdddPetModal>("Add new Pet", dialogOptions);
+            var result = await dialog.Result;
+
+            if (result != null && !result.Canceled)
+            {
+                await RefreshGrid();
+            }
+        }
+
+
+        private string GetHealthStatusBadgeClass(HealthStatus status)
+        {
+            return status switch
+            {
+                HealthStatus.GoodHealth => "bg-success",
+                HealthStatus.Vaccinated => "bg-primary",
+                HealthStatus.SpecialMedicalNeeds => "bg-warning",
+                HealthStatus.UnderTreatment => "bg-danger",
+                _ => "bg-secondary"
+            };
+        }
+
+        private async Task DeletePet(int id)
+        {
+            try
+            {
+                Loader.Show();
+                await petAPI.DeletePetAsync(id);
+
+                // grid refresh with db calling
+                var petToRemove = petsList.FirstOrDefault(p => p.Id == id);
+                if (petToRemove != null)
+                {
+                    petsList = petsList.Where(p => p.Id != id).ToList();
+                }
+
+                Snackbar.Add($"Pet info deleted successfully", Severity.Success);
+                StateHasChanged();
+            }
+            catch
+            {
+                Loader.Hide();
+                Snackbar.Add($"Not able to delete pet info", Severity.Error);
+            }
+            finally
+            {
+                Loader.Hide();
+            }
+        }
+
+        private async Task RefreshGrid()
+        {
+            Loader.Show();
+            try
+            {
+                var result = await petAPI.GetAllPetsAsync();
+
+                petsList = result is null || !result.Any() ? new List<PetViewModel>() : result;
+                StateHasChanged();
+            }
+            catch
+            {
+                Loader.Hide();
+                Snackbar.Add($"Not fetched all pets information", Severity.Error);
+            }
+            finally
+            {
+                Loader.Hide();
+            }
+        }
+
+        #endregion methods
+    }
+}
